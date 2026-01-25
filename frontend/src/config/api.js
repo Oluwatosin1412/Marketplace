51a781d670d66fb74606774f50defe54381d6487
Akpofurele Victor <akpofurelev@gmail.com>
2026-01-25 17:37:00 +0100

    Solved domain issue

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = "https://marketplace-two-rosy.vercel.app/api";
-export const API_BASE_URL = "https://marketplace-y6en.onrender.com/api";
+export const API_BASE_URL = "https://marketplace-two-rosy.vercel.app/api";
+// export const API_BASE_URL = "https://marketplace-y6en.onrender.com/api";
